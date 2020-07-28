c5d64077508ad3c83677008af321ef09179eb576
niles87 <nbing19@gmail.com>
2020-07-27 21:05:23 -0700

    adding the rest of the starter files

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const exphbs = require("express-handlebars");
 const sequelize = require("./config/config");
 const app = express();
 const PORT = process.env.PORT || 4000;
+// const routes = require("./routes");
 
-app.engine("handlebars", exphbs());
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(require("./routes/"));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
